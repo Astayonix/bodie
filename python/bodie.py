fc0acfec4ae5efdf995ae682efefecc4597d83d3
Astayonix <jessica.l.burns@gmail.com>
2016-04-11 06:23:54 -0700

    Trying to prettify the JSON

--- python/bodie.py
+++ python/bodie.py
@@ -1,13 +1,20 @@
 import os
 #import sys
+import json
 from slacker import Slacker
 
 slack = Slacker(os.environ['SLACK_OAUTH_ACCESS_TOKEN'])
 
 # Send a message to #team-chips-and-crisps channel
 #slack.chat.post_message('#team-chips-and-crisps', 'Hello fellow slackers, this is our bot!')
-channel_list = slack.channels.info() 
-print channel_list.body
+
+#trying to prettyprint the json returned by channels
+channel_obj = slack.channels.list()
+channels = channel_obj.body
+json_str_channel_list = json.dumps(channels)
+channels = json.loads(json_str_channel_list)
+print channels
+
 # Get users list
 #response = slack.users.list()
 # print response
